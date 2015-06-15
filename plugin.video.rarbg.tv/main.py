3faf7ae8b288ff0f139dec1a501401a3e9ec2547
Roman Miroshnychenko (Work) <romanvm@yandex.ru>
2015-06-15 11:56:16 +0300

    Fixed a bug with missing 'Download torrent...' context menu item; moved actions mapping to 'actions' module;fixed a bug with incorrect rating parsing in episodes list

--- plugin.video.rarbg.tv/main.py
+++ plugin.video.rarbg.tv/main.py
@@ -4,14 +4,6 @@
 # Created on: 13.05.2015
 # License: GPL v.3 https://www.gnu.org/copyleft/gpl.html
 
-from simpleplugin import Plugin
-from libs import actions
+from libs.actions import plugin
 
-plugin = Plugin()
-plugin.actions['root'] = actions.root
-plugin.actions['episode_list'] = actions.episode_list
-plugin.actions['search_episodes'] = actions.search_episodes
-plugin.actions['episode'] = actions.episode
-plugin.actions['my_shows'] = actions.my_shows
-if __name__ == '__main__':
-    plugin.run(content='tvshows')
+plugin.run(content='tvshows')
