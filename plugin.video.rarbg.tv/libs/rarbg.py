e223461fe7e10b3b1ec1ad5980b8b6a6ab9cacdf
Roman Miroshnychenko (Work) <romanvm@yandex.ru>
2015-06-18 14:40:21 +0300

    Added rarbg and thetvdb modules, added controls to settings.xml

--- plugin.video.rarbg.tv/libs/rarbg.py
+++ plugin.video.rarbg.tv/libs/rarbg.py
@@ -5,18 +5,31 @@
 # License: GPL v.3 https://www.gnu.org/copyleft/gpl.html
 # Rarbg API docs: http://torrentapi.org/apidocs_v2.txt
 
-from simpleplugin import Plugin
 from webclient import load_page
 
 _API = 'https://torrentapi.org/pubapi_v2.php'
-_plugin = Plugin()
 
 
-@_plugin.cached(15)
 def get_token():
     """
     Get a token to access Rarbg API
-    The token will expire in 15 mins
+    The token will expire in 15 min
     :return:
     """
-    return load_page(_API, data={'get_token': 'get_token'}, headers={'content-type': 'application/json'})['token']
+    data = {'get_token': 'get_token'}
+    return load_page(_API, data=data, headers={'content-type': 'application/json'})['token']
+
+
+def get_torrents(token, **kwargs):
+    """
+    Get the list of recent TV episode torrents with extended data
+    :param token: str - Rarbg API token
+    :param kwargs: dict - keyword Rarbg API parameters
+    :return: list - the list of torrents
+    """
+    data = {'token': token, 'format': 'json_extended'}
+    data.update(kwargs)
+    try:
+        return load_page(_API, data=data, headers={'content-type': 'application/json'})['torrent_results']
+    except KeyError:
+        return []
--- plugin.video.rarbg.tv/libs/thetvdb.py
+++ plugin.video.rarbg.tv/libs/thetvdb.py
@@ -4,4 +4,25 @@
 # Created on: 17.06.2015
 # License: GPL v.3 https://www.gnu.org/copyleft/gpl.html
 
+from xml.dom.minidom import parseString
+from webclient import load_page
 
+_GET_SERIES = 'http://thetvdb.com/api/GetSeries.php'
+_GET_BY_ID = 'http://thetvdb.com/api/GetSeriesByRemoteID.php'
+_GRAPHICS = 'http://thetvdb.com/banners/'
+
+
+def get_series_by_imdbid(imdbid):
+    """
+    Get basic TV show info from TheTVDB
+    :param imdbid:
+    :return:
+    """
+    document = parseString(load_page(_GET_BY_ID, data={'imdbid' : imdbid}))
+    try:
+        return {'tvshowtitle': document.getElementsByTagName('SeriesName')[0].firstChild.nodeValue,
+                'plotoutline': document.getElementsByTagName('Overview')[0].firstChild.nodeValue,
+                'premiered': document.getElementsByTagName('FirstAired')[0].firstChild.nodeValue,
+                'banner': _GRAPHICS + document.getElementsByTagName('banner')[0].firstChild.nodeValue}
+    except IndexError:
+        return {}
