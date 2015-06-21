d6847eeb316289d63090cb3046c7fa6e223e50da
Roman Miroshnychenko <romanvm@yandex.ru>
2015-06-21 14:52:56 +0300

    Moved entering search query to a separate function for DRY, removed storing cookies

--- plugin.video.rarbg.tv/libs/webclient.py
+++ plugin.video.rarbg.tv/libs/webclient.py
@@ -4,7 +4,6 @@
 # Created on: 15.05.2015
 # License: GPL v.3 https://www.gnu.org/copyleft/gpl.html
 
-import urlparse
 import requests
 from xbmc import LOGDEBUG
 from simpleplugin import Plugin
@@ -31,19 +30,15 @@ def load_page(url, method='get', data=None, headers=None):
     if headers is not None:
         for key, value in headers.iteritems():
             request_headers[key] = value
-    contents = None
-    with _plugin.get_storage('cookies.pcl') as cookie_jar:
-        cookies = cookie_jar.get(urlparse.urlparse(url).hostname)
-        if method == 'get':
-            response = requests.get(url, params=data, headers=request_headers, verify=False, cookies=cookies)
-        elif method == 'post':
-            response = requests.post(url, data=data, headers=request_headers, verify=False, cookies=cookies)
-        else:
-            raise RuntimeError('Invalid load_page method!')
-        cookie_jar[urlparse.urlparse(url).hostname] = response.cookies.get_dict()
-        if response.headers['content-type'] == 'application/json':
-            contents = response.json()
-        else:
-            contents = response.text
-        _plugin.log(response.text, LOGDEBUG)
+    if method == 'get':
+        response = requests.get(url, params=data, headers=request_headers, verify=False)
+    elif method == 'post':
+        response = requests.post(url, data=data, headers=request_headers, verify=False)
+    else:
+        raise RuntimeError('Invalid load_page method!')
+    if response.headers['content-type'] == 'application/json':
+        contents = response.json()
+    else:
+        contents = response.text
+    _plugin.log(response.text, LOGDEBUG)
     return contents
