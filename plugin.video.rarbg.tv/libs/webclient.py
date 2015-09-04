402f0fa69fd8f020a59a1ce4d151b60736662023
Roman Miroshnychenko <romanvm@yandex.ru>
2015-09-04 14:50:07 +0300

    Changed doctstring format to epytext

--- plugin.video.rarbg.tv/libs/webclient.py
+++ plugin.video.rarbg.tv/libs/webclient.py
@@ -16,11 +16,11 @@ def load_page(url, method='get', data=None, headers=None):
     """
     Web-client
 
-    :param url: str - URL
-    :param method: str - 'get' or 'post' methdos, other methods are not supported.
-    :param data: dict - data to be sent to a server
-    :param headers: dict - custom headers to replace/add to default ones
-    :return: unicode or dict - response contents or a dictionary of json-decoded data
+    @param url: str - URL
+    @param method: str - 'get' or 'post' methdos, other methods are not supported.
+    @param data: dict - data to be sent to a server
+    @param headers: dict - custom headers to replace/add to default ones
+    @return: unicode or dict - response contents or a dictionary of json-decoded data
     """
     _plugin.log('URL: {0}, params: {1}'.format(url, str(data)))
     request_headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:37.0) Gecko/20100101 Firefox/37.0',
