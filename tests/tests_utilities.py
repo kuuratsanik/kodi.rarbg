005b720eee726e92f09d97f03468f23fd2a9b5b9
Roman Miroshnychenko (Work) <romanvm@yandex.ru>
2016-02-17 13:06:44 +0200

    Refactored tests_utilities

--- tests/tests_utilities.py
+++ tests/tests_utilities.py
@@ -6,7 +6,7 @@
 import os
 import sys
 from unittest import TestCase
-from mock import MagicMock
+from mock import MagicMock, patch
 from libs.exceptions import Http404Error
 
 __all__ = ['LoadPageTestCase']
@@ -14,8 +14,7 @@ __all__ = ['LoadPageTestCase']
 basedir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, os.path.join(basedir, 'plugin.video.rarbg.tv'))
 
-mock_requests = MagicMock()
-sys.modules['requests'] = mock_requests
+sys.modules['requests'] = MagicMock()
 sys.modules['xbmc'] = MagicMock()
 sys.modules['simpleplugin'] = MagicMock()
 
@@ -30,33 +29,33 @@ HEADERS = {
 }
 
 
+@patch('libs.utilities.requests.get')
 class LoadPageTestCase(TestCase):
     def setUp(self):
         self.mock_response = MagicMock()
-        self.mock_get = MagicMock()
-        self.mock_get.return_value = self.mock_response
-        mock_requests.get = self.mock_get
 
-    def test_load_page_json(self):
+    def test_load_page_json(self, mock_get):
         self.mock_response.headers = {'content-type': 'application/json'}
         self.mock_response.json.return_value = {'foo': 'bar'}
+        mock_get.return_value = self.mock_response
         result = load_page('foo')
-        self.mock_get.assert_called_with('foo', params=None, headers=HEADERS, verify=False)
+        mock_get.assert_called_with('foo', params=None, headers=HEADERS, verify=False)
         self.assertEqual(result['foo'], 'bar')
 
-    def test_load_page_xml(self):
+    def test_load_page_xml(self, mock_get):
         self.mock_response.headers = {'content-type': 'application/xml'}
         self.mock_response.text = '<foo>Bar</foo>'
+        mock_get.return_value = self.mock_response
         result = load_page('bar')
         self.assertEqual(result, '<foo>Bar</foo>')
 
-    def test_page_not_found(self):
+    def test_page_not_found(self, mock_get):
         self.mock_response.status_code = 404
+        mock_get.return_value = self.mock_response
         self.assertRaises(Http404Error, load_page, 'foo')
 
-    def test_pass_headers(self):
-        self.mock_get.reset_mock()
+    def test_pass_headers(self, mock_get):
         headers = {'content-type': 'application/json'}
         HEADERS.update(headers)
         load_page('foo', headers=headers)
-        self.mock_get.assert_called_with('foo', params=None, headers=HEADERS, verify=False)
+        mock_get.assert_called_with('foo', params=None, headers=HEADERS, verify=False)
